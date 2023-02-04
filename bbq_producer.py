262df2a7e95b2cb8cbb6f5c3b94efe6708261094
Sammie <s520250@nwmissouri.edu>
2023-02-03 18:53:36 -0600

    02-03-2023 - Created Module 5 Repo

---
+++ bbq_producer.py
@@ -0,0 +1,10 @@
+"""
+Author: Sammie Bever
+Date: February 3, 2023 
+Class: Streaming Data 
+Assignment: Module 05
+
+This program uses producers, consumers, and task queues (RabbitMQ). 
+It reads data from the smoker-temps.csv file for smart smokers.
+
+"""
